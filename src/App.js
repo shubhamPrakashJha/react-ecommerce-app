b3d8b04667fc2179e84ed0005b1cc19438a6bfe9
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2020-02-22 23:46:53 +0530

    add hompage component to

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import HomePage from "./homepage.component";
+
 import "./App.css";
 
 function App() {
-    return <div>Hello World</div>;
+	return <HomePage />;
 }
 
 export default App;
