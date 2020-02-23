9abdd9bc129e06ce092f153c18f87d33397ec63a
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2020-02-23 21:25:27 +0530

    Integrated firebase google sign in and sign out page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,10 +51,12 @@ class SignIn extends Component {
 						label="Password"
 						required
 					/>
-					<CustomButton type="submit">Sign In</CustomButton>
-					<CustomButton onClick={signInWithGoogle}>
-						Sign in with Google
-					</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit">Sign In</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
