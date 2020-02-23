7899a4c807ca82af2dfe1943219d9d9179d4356c
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2020-02-23 23:23:27 +0530

    add first time user user to firebase store

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import LoginPage from "./pages/loginpage/login.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
 	constructor(props) {
@@ -21,10 +21,29 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			// this.setState({ currentUser: user });
+			// console.log(user);
+			// createUserProfileDocument(user);
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-			console.log(user);
+				userRef.onSnapshot(snapshot => {
+					this.setState(
+						{
+							currentUser: {
+								id: snapshot.id,
+								...snapshot.data()
+							}
+						},
+						() => {
+							console.log(snapshot, snapshot.data(), this.state);
+						}
+					);
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
