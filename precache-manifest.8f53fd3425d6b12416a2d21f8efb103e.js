da92ff056f25b2d643b5cbb6be5478b394d5f820
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2020-02-23 11:47:19 +0530

    Updates

---
+++ precache-manifest.8f53fd3425d6b12416a2d21f8efb103e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000ad8359fb568d2dfa1a7cd22d211ed",
+    "url": "/react-ecommerce-app/index.html"
+  },
+  {
+    "revision": "b6e670830898d801a51a",
+    "url": "/react-ecommerce-app/static/css/main.c18925ec.chunk.css"
+  },
+  {
+    "revision": "ac9bf2f661e244b79ecb",
+    "url": "/react-ecommerce-app/static/js/2.2de14585.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-ecommerce-app/static/js/2.2de14585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e670830898d801a51a",
+    "url": "/react-ecommerce-app/static/js/main.b4672bd7.chunk.js"
+  },
+  {
+    "revision": "742c820b4fa1c3486197",
+    "url": "/react-ecommerce-app/static/js/runtime-main.b1bfd370.js"
+  }
+]);
